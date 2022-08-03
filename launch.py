c360e37c5523b9e7c6d4d61172ba1a4bf867669f
KremenTech01 <213>
2022-08-03 13:53:15 +0300

    v3

--- launch.py
+++ launch.py
@@ -1,18 +1,13 @@
-from logic import BigClass
+from logic import Parser
 
 try:
     page_count = int(input('Enter number of pages \n'))
 except ValueError:
     page_count = 1
 
-if __name__ == "__main__":
-    basic = BigClass(''
-                     'https://auto.ria.com/uk/search/?indexName=auto&country.import.usa.not=-1&price.currency=1&abroad.not=0&custom.not=1&',
-                     page_count, {
-                         "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36"
-                     })
+url = 'https://auto.ria.com/uk/search/?indexName=auto&country.import.usa.not=-1&price.currency=1&abroad.not=0&custom.not=1&'
+header = {
+         "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36"
+        }
 
-basic.get_all_cars()
-basic.get_all_info()
-basic.get_serialized_content()
-basic.create_db()
+Parser(url, page_count, header).start()
