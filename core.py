71399c2aca0d54a046d09e93cfed74713812f72c
KremenTech01 <213>
2022-08-03 14:32:50 +0300

    v3. refactor. rename variables

--- core.py
+++ core.py
@@ -13,9 +13,9 @@ tz_here = pytz.timezone("Europe/Kiev")
 
 @dataclass
 class Regulars:
-    objem: str = r"\d[.]\d\d?\sл\s? | \d\sл\s?"
-    korobka: str = r"(?i)(\W|^)(Автомат|Ручна\s[/]\sМеханіка|Варіатор)(\W|$)"
-    toplivo: str = r"(?i)(\W|^)(Бензин|Дизель|Електро|Газ\s/\sБензин|Гiбрид)(\W|$)"
+    engine_capacity: str = r"\d[.]\d\d?\sл\s? | \d\sл\s?"
+    transmission: str = r"(?i)(\W|^)(Автомат|Ручна\s[/]\sМеханіка|Варіатор)(\W|$)"
+    fuel_type: str = r"(?i)(\W|^)(Бензин|Дизель|Електро|Газ\s/\sБензин|Гiбрид)(\W|$)"
 
 
 class Parser(object):
@@ -48,16 +48,16 @@ class Parser(object):
     def get_cars_urls(self):
         for result_set in self.cars:
             url = result_set.find("a", class_="m-link-ticket").attrs.get("href")
-            data = result_set.find("div", class_="footer_ticket").find("span").attrs
-            self.cars_urls[f"{url}"] = data
+            date = result_set.find("div", class_="footer_ticket").find("span").attrs
+            self.cars_urls[f"{url}"] = date
 
     def get_all_info(self):
-        for url, data in self.cars_urls.items():
+        for url, date in self.cars_urls.items():
             req = requests.get(url, headers=self.header)
             content = BeautifulSoup(req.text, "html.parser")
-
+            date = date
             self.list_with_cars.append(
-                Serializer(content=content, data=data, url=url).start()
+                Serializer(content=content, date=date, url=url).start()
             )
 
     def bulk_create_db(self):
@@ -65,21 +65,21 @@ class Parser(object):
 
 
 class Serializer(object):
-    def __init__(self, content: BeautifulSoup, data: str, url: str):
+    def __init__(self, content: BeautifulSoup, date: str, url: str):
         self.content = content
-        self.data = data
+        self.date = date
         self.url = url
         self.name_model_year = None
-        self.probeg = None
+        self.mileage = None
         self.city = None
         self.tech_info = None
-        self.probeg_per_year = None
-        self.objem = None
-        self.korobka = None
-        self.toplivo = None
-        self.nomer = None
+        self.mileage_per_year = None
+        self.engine_capacity = None
+        self.transmission = None
+        self.fuel_type = None
+        self.reg_number = None
         self.vin = None
-        self.opisanie = None
+        self.description = None
         self.price_usd = None
         self.price_uah = None
         self.current_car = {}
@@ -87,13 +87,13 @@ class Serializer(object):
 
     def start(self):
         self.get_name_model_year()
-        self.get_probeg()
+        self.get_mileage()
         self.get_city()
         self.get_technical_info()
-        self.get_probeg_per_year()
+        self.get_mileage_per_year()
         self.get_engine_info()
         self.get_registration_info()
-        self.get_opisanie()
+        self.get_description()
         self.get_prices()
         self.create_dict()
         self.to_tuple()
@@ -107,16 +107,16 @@ class Serializer(object):
         except AttributeError:
             self.name_model_year = None
 
-    def get_probeg(self):
+    def get_mileage(self):
         try:
-            self.probeg = (
+            self.mileage = (
                 self.content.find("div", class_="technical-info", id="details")
                 .find("dd", class_="mhide")
                 .find("span", class_="argument")
                 .text
             )
         except AttributeError:
-            self.probeg = None
+            self.mileage = None
 
     def get_city(self):
         try:
@@ -134,41 +134,41 @@ class Serializer(object):
         except AttributeError or ValueError:
             self.tech_info = None
 
-    def get_probeg_per_year(self):
+    def get_mileage_per_year(self):
         try:
-            self.probeg_per_year = round(
-                int(self.probeg.split(" ")[0])
+            self.mileage_per_year = round(
+                int(self.mileage.split(" ")[0])
                 / (int(datetime.now().year) - int(self.name_model_year[-1])),
                 2,
             )
         except ValueError:
-            self.probeg_per_year = None
+            self.mileage_per_year = None
         except ZeroDivisionError:
-            self.probeg_per_year = round(int(self.probeg.split(" ")[0]) / 1, 2)
+            self.mileage_per_year = round(int(self.mileage.split(" ")[0]) / 1, 2)
 
     def get_engine_info(self):
         try:
-            self.objem = re.search(Regulars.objem, str(self.tech_info)).group(0)
+            self.engine_capacity = re.search(Regulars.engine_capacity, str(self.tech_info)).group(0)
         except AttributeError:
-            self.objem = None
+            self.engine_capacity = None
         try:
-            self.korobka = re.search(Regulars.korobka, str(self.tech_info)).group(0)
+            self.transmission = re.search(Regulars.transmission, str(self.tech_info)).group(0)
         except AttributeError:
-            self.korobka = None
+            self.transmission = None
         try:
-            self.toplivo = re.search(Regulars.toplivo, str(self.tech_info)).group(0)
+            self.fuel_type = re.search(Regulars.fuel_type, str(self.tech_info)).group(0)
         except AttributeError:
-            self.toplivo = None
+            self.fuel_type = None
 
     def get_registration_info(self):
         try:
-            self.nomer = (
+            self.reg_number = (
                 self.content.find("div", class_="t-check")
                 .find("span", class_="state-num ua")
                 .text
             )
         except AttributeError:
-            self.nomer = "X X X X"
+            self.reg_number = "X X X X"
         try:
             self.vin = (
                 self.content.find("div", class_="t-check")
@@ -185,11 +185,11 @@ class Serializer(object):
             except AttributeError:
                 self.vin = None
 
-    def get_opisanie(self):
+    def get_description(self):
         try:
-            self.opisanie = self.content.find("div", class_="full-description").text
+            self.description = self.content.find("div", class_="full-description").text
         except AttributeError:
-            self.opisanie = None
+            self.description = None
 
     def get_prices(self):
         try:
@@ -211,21 +211,21 @@ class Serializer(object):
     def create_dict(self):
         self.current_car = {
             "id": self.url.split(".html")[0].split("_")[-1],
-            "marka": self.name_model_year[0],
+            "name": self.name_model_year[0],
             "model": str.join(
                 " ", self.name_model_year[1 : len(self.name_model_year) - 1]
             ),
             "year": self.name_model_year[-1],
-            "probeg": self.probeg,
-            "probeg_per_year": f"{self.probeg_per_year} тис.км / рiк",
+            "mileage": self.mileage,
+            "mileage_per_year": f"{self.mileage_per_year} тис.км / рiк",
             "city": self.city,
-            "korobka": self.korobka,
-            "toplivo": self.toplivo,
-            "objem": self.objem,
-            "nomer": str.join(" ", self.nomer.split()[0:3]),
+            "transmission": self.transmission,
+            "fuel_type": self.fuel_type,
+            "engine_capacity": self.engine_capacity,
+            "reg_number": str.join(" ", self.reg_number.split()[0:3]),
             "vin": self.vin,
-            "opisanie": self.opisanie,
-            "data_objavi": self.data.get("data-add-date"),
+            "description": self.description,
+            "publication_date": self.date.get("data-add-date"),
             "price_usd": self.price_usd,
             "price_uah": self.price_uah,
             "url": self.url,
@@ -246,19 +246,19 @@ class Database(object):
         self.cur.execute(
             """CREATE TABLE IF NOT EXISTS cars(
                     id INTEGER PRIMARY KEY ,
-                    marka TEXT,
+                    name TEXT,
                     model TEXT,
                     year TEXT,
-                    probeg TEXT,
-                    probeg_per_year TEXT,
+                    mileage TEXT,
+                    mileage_per_year TEXT,
                     city TEXT,
-                    korobka TEXT,
-                    toplivo TEXT,
-                    objem TEXT,
-                    nomer TEXT NULL,
+                    transmission TEXT,
+                    fuel_type TEXT,
+                    engine_capacity TEXT,
+                    reg_number TEXT NULL,
                     vin TEXT NULL,
-                    opisanie TEXT,
-                    data_objavi TEXT,
+                    description TEXT,
+                    publication_date TEXT,
                     price_usd TEXT,
                     price_uah TEXT,
                     url TEXT)
@@ -268,9 +268,9 @@ class Database(object):
     def save_data(self):
         self.cur.executemany(
             "INSERT OR REPLACE INTO cars(id, "
-            "marka, model, year, probeg, probeg_per_year,"
-            "city, korobka, toplivo, objem, nomer, vin,"
-            "opisanie, data_objavi, price_usd, price_uah, url)"
+            "name, model, year, mileage, mileage_per_year,"
+            "city, transmission, fuel_type, engine_capacity, reg_number, vin,"
+            "description, publication_date, price_usd, price_uah, url)"
             "VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)",
             self.list_with_cars,
         )
